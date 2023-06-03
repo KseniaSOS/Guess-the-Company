de405eaf4773438670d555e72da204a74d7f511b
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-06-03 18:25:48 +0000

    Add js file with answers

---
+++ assets/js/questions.js
@@ -0,0 +1,252 @@
+const questions = [
+    {
+        question: "Just Do It",
+        answers: [
+            {text : "Puma", correct: false},
+            {text : "Adidas", correct: false},
+            {text : "Nike", correct: true},
+            {text : "NB", correct: false},
+        ]
+    
+    },
+    {
+        question: "Believe In Your Smell",
+        answers: [
+            {text : "Rexona", correct: false},
+            {text : "Old Spice", correct: true},
+            {text : "Degree", correct: false},
+            {text : "Mitchum", correct: false},
+        ]
+    
+    },
+    {
+        question: "The taste of a new generation",
+        answers: [
+            {text : "Fanta", correct: false},
+            {text : "Coca Cola", correct: false},            
+            {text : "Sprite", correct: false},
+            {text : "Pepsi", correct: true},
+        ]
+    
+    },
+    {
+        question: "A Passion For the Road",
+        answers: [
+            {text : "Ford", correct: false},
+            {text : "Opel", correct: false},
+            {text : "Mazda", correct: true},
+            {text : "Hyndai", correct: false},
+        ]
+    
+    },
+    {
+        question: "Eat fresh",
+        answers: [
+            {text : "Subway", correct: true},
+            {text : "McDonald's", correct: false},
+            {text : "KFC", correct: false},            
+            {text : "Burger King", correct: false},
+        ]
+    
+    },
+    {
+        question: "Melts in your mouth, not in your hand",
+        answers: [
+            {text : "ChocZero", correct: false},
+            {text : "M&Ms", correct: true},
+            {text : "Built Bar", correct: false},           
+            {text : "Skittles", correct: false},
+        ]
+    
+    },
+    {
+        question: "I'm lovin' it.",
+        answers: [
+            {text : "KFC", correct: false},
+            {text : "Subway", correct: false},
+            {text : "McDonald's", correct: true},
+            {text : "Burger King", correct: false},
+        ]
+    
+    },
+    {
+        question: "The happiest place on earth.",
+        answers: [
+            {text : "Disneyland", correct: false},
+            {text : "Universal Studios", correct: false},
+            {text : "Lego Land", correct: true},
+            {text : "Tivoli Gardens", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+    {
+        question: "",
+        answers: [
+            {text : "", correct: false},
+            {text : "", correct: false},
+            {text : "", correct: true},
+            {text : "", correct: false},
+        ]
+    
+    },
+]
