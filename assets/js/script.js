cce8a286cfd43144ab5d3c9e244109130545aa43
Ksenia Sossin <sossin.ksenia@gmail.com>
2023-06-07 21:24:22 +0000

    add play again button with associated event listener in game page, fix issue with random selection of questions, fix issue with score reset, make next button visible only when question is answered, add set function for score instead of increment in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,49 @@
 /** Wait for the DOM to finish loading the page before running the game */
 document.addEventListener("DOMContentLoaded", startGame);
 
-/** Variables */
+/** Constants */
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const playAgainButton = document.getElementById("play-again-btn");
+
+/** Variables */
 let score = document.getElementById("score");
-
-
-
 let currentQuestionIndex = 0;
-score = 0;
+let shuffledQuestions;
 
 
+function shuffle(array) {
+    let shuffledArray = array
+    for (let i = shuffledArray.length - 1; i > 0; i--) 
+    {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = shuffledArray[i];
+        shuffledArray[i] = shuffledArray[j];
+        shuffledArray[j] = temp;
+    }
+    return shuffledArray;
+}
 
 /** Starts the game*/
 function startGame(){
+    // initialize variables
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
+    setScore(score);
+    // randomly shuffle the questions array
+    shuffledQuestions = shuffle(questions);
+    showQuestion(currentQuestionIndex); 
+    nextButton.style.display = "none";
+    playAgainButton.style.display = "none";
 }
 
 //the code was taken from youtube channel: https://shorturl.at/hrIV9
 /** Shows random questions with answers from the question list. Add event listeners to answers buttons */
-function showQuestion(){
+function showQuestion(questionIndex){
     resetState();
-    let currentQuestion = questions[Math.floor(Math.random() * questions.length)];
-    let questionNo = currentQuestionIndex + 1;
+    let currentQuestion = shuffledQuestions[questionIndex];
+    let questionNo = questionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.
     question;
 
@@ -45,9 +61,9 @@ function showQuestion(){
 }
 
 
-/** Reset the privious questions */
+/** Reset the previous questions */
 function resetState() {        
-        
+        nextButton.style.display = "none";
         while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild);
     }
@@ -63,7 +79,8 @@ function checkAnswer(e) {
     if(answerCorrect) {
         selectedBtn.classList.add("correct");
         score++;
-        incrementScore();
+        setScore(score);
+        // incrementScore();
     } else {
         selectedBtn.classList.add("incorrect");
     }
@@ -72,6 +89,7 @@ function checkAnswer(e) {
             button.classList.add("correct");
         }
         button.disabled = true;
+        nextButton.style.display = "block";
     }); 
            
 }
@@ -83,41 +101,44 @@ function checkAnswer(e) {
 
 // //////
 
-/** displays the score  at the and of questions*/
+/** displays the score when all questions were processed*/
 function showScore(){
     resetState();
     questionElement.innerHTML = `WELL DONE! You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
-}
-
-// code from the Love Math project//
- /**Gets the current score from the DOM and increments it by 1
- */
-function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
+    nextButton.style.display = "none";
+    playAgainButton.style.display = "block";
 }
 
 
-/** displays questions one by one, if there is no more, it will display the score*/
-function handleNextButton(){
-   currentQuestionIndex++;
-   if(currentQuestionIndex < questions.length){
-    showQuestion();
-   }else{
-    showScore();
-   }
+ /** set the current score in the DOM */
+function setScore(inputScore) {
+    document.getElementById("score").innerText = inputScore;
+}
 
    
-}
+// }
 
 /** Add event listeners to next button. Checks if all questions are displayed, than restart a new game  */
 nextButton.addEventListener("click", ()=>{
     if(currentQuestionIndex < questions.length){
-        handleNextButton();
+        showQuestion(currentQuestionIndex);
+        currentQuestionIndex++;
+        // handleNextButton();
     } else{
-        startGame();
+        if (currentQuestionIndex == questions.length)
+        {
+            showScore();
+        }
+        else
+        {
+            startGame();
+        }
     }
+})
+
+/** Add event listeners to the play again button.  */
+playAgainButton.addEventListener("click", ()=>{
+    startGame();
 })
 
 
