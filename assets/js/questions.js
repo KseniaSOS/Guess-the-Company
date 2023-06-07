76711eef8242ed901ac6b5ec809363940093acff
KseniaSOS <>
2023-06-07 10:53:32 +0000

    Add more questions to JS file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,7 +6,7 @@ const questions = [
             {text : "Puma", correct: false},
             {text : "Adidas", correct: false},
             {text : "Nike", correct: true},
-            {text : "NB", correct: false},
+            {text : "New Balance", correct: false},
         ]
     
     },
@@ -24,7 +24,7 @@ const questions = [
         question: "The taste of a new generation",
         answers: [
             {text : "Fanta", correct: false},
-            {text : "Coca Cola", correct: false},            
+            {text : "Coca-Cola", correct: false},            
             {text : "Sprite", correct: false},
             {text : "Pepsi", correct: true},
         ]
@@ -51,7 +51,7 @@ const questions = [
     
     },
     {
-        question: "Melts in your mouth, not in your hand",
+        question: "Melts in your mouth, not in your hand.",
         answers: [
             {text : "ChocZero", correct: false},
             {text : "M&Ms", correct: true},
@@ -80,174 +80,224 @@ const questions = [
         ]
     
     },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    {
+        question: "Think different.",
+        answers: [
+            {text : "Microsoft", correct: false},
+            {text : "Dell", correct: false},
+            {text : "Apple", correct: true},
+            {text : "Samsung", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Bbecause you’re worth it.",
+        answers: [
+            {text : "Estee Lauder", correct: false},
+            {text : "Maybelline", correct: false},            
+            {text : "Lancome", correct: false},
+            {text : "L'Oreal", correct: true},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "The ultimate driving machine",
+        answers: [
+            {text : "Audi", correct: false},
+            {text : "Honda", correct: false},
+            {text : "Hyndai", correct: false},
+            {text : "BMW", correct: true},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "It keeps going, and going, and going…",
+        answers: [
+            {text : "Duracell", correct: false},
+            {text : "Panasonic", correct: false},
+            {text : "Energizer", correct: true},
+            {text : "Royovac", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Let’s go places",
+        answers: [
+            {text : "Acura", correct: false},
+            {text : "Toyota", correct: true},
+            {text : "Chevrolet", correct: false},            
+            {text : "Lexus", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Betcha can’t eat just one",
+        answers: [
+            {text : "Lay's", correct: true},
+            {text : "Cheetos", correct: false},
+            {text : "Pringles", correct: false},            
+            {text : "Doritos", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "All the news that’s fit to print",
+        answers: [
+            {text : "USA Today", correct: false},
+            {text : "The Wall Street Journal", correct: false},
+            {text : "The New York Times", correct: true},
+            {text : "Bild", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Imagination at work",
+        answers: [
+            {text : "Apple", correct: false},
+            {text : "Disney Studios", correct: false},            
+            {text : "Boeing", correct: false},
+            {text : "General Electric", correct: true},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Impossible is nothing",
+        answers: [
+            {text : "Puma", correct: false},
+            {text : "Nike", correct: false},
+            {text : "Adidas", correct: true},
+            {text : "New Balance", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "The best a man can get",
+        answers: [
+            {text : "Braun", correct: false},
+            {text : "Gilette", correct: true},
+            {text : "Shick", correct: false},
+            {text : "BIC", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Play. Laugh. Grow.",
+        answers: [
+            {text : "Barbie", correct: false},
+            {text : "Fisher-Price", correct: true},
+            {text : "Lego", correct: false},            
+            {text : "Playmobil", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Taste the Rainbow",
+        answers: [
+            {text : "Chupa Chups", correct: false},
+            {text : "M&M", correct: false},
+            {text : "Haribo", correct: false},
+            {text : "Skittles", correct: true},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "The taste of paradise",
+        answers: [
+            {text : "Bounty", correct: true},
+            {text : "Snickers", correct: false},
+            {text : "Twix", correct: false},            
+            {text : "Mars", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Probably the best beer in the world",
+        answers: [
+            {text : "Guinness", correct: false},
+            {text : "Corona", correct: false},
+            {text : "Carlsberg", correct: true},
+            {text : "Heineken", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Where will tomorrow take you?",
+        answers: [
+            {text : "Emirates", correct: true},
+            {text : "Turkish Airlines", correct: false},
+            {text : "Lufthansa", correct: false},
+            {text : "Qatar Airways", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "Obey your thirst.",
+        answers: [
+            {text : "Sprite", correct: true},
+            {text : "Izze", correct: false},
+            {text : "Coca-Cola", correct: false},            
+            {text : "Pepci", correct: false},
+        ]
     
-    // },
-    // {
-    //     question: "",
-    //     answers: [
-    //         {text : "", correct: false},
-    //         {text : "", correct: false},
-    //         {text : "", correct: true},
-    //         {text : "", correct: false},
-    //     ]
+    },
+    {
+        question: "The make-up of make-up artists.",
+        answers: [
+            {text : "Tom Ford", correct: false},
+            {text : "Lancome", correct: false},
+            {text : "Max Factor", correct: true},
+            {text : "Dior", correct: false},
+        ]
     
-    // },
+    },
+    {
+        question: "A Better Life, Abetter World.",
+        answers: [
+            {text : "Panasonic", correct: true},
+            {text : "Sony", correct: false},
+            {text : "Canon", correct: false},            
+            {text : "Lumix", correct: false},
+        ]
+    
+    },
+    {
+        question: "Buy it. Sell it. Love it.",
+        answers: [            
+            {text : "Amazon", correct: false},
+            {text : "Etsy", correct: false},            
+            {text : "Facebook Marketplace", correct: false},
+            {text : "eBay", correct: true},
+        ]
+    
+    },
+    {
+        question: "Connecting People",
+        answers: [
+            {text : "Nokia", correct: true},
+            {text : "Motorola", correct: false},
+            {text : "Apple", correct: false},            
+            {text : "Samsung", correct: false},
+        ]
+    
+    },
+    {
+        question: "Spend a little, live a lot",
+        answers: [
+            {text : "Aldi", correct: true},
+            {text : "Penny", correct: false},
+            {text : "Lidl", correct: false},            
+            {text : "Maxima", correct: false},
+        ]
+    
+    },
+    {
+        question: "Insurance solutions from A to Z",
+        answers: [            
+            {text : "Generali", correct: false},
+            {text : "CNA", correct: false},  
+            {text : "Allianz", correct: true},          
+            {text : "Zurich", correct: false},
+        ]
+    
+    },
 ];
